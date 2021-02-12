83f0c496d863b09f109472ce142f538e5aeab96e
s22047-pj <s22047@pjwstk.edu.pl>
2021-02-12 23:13:07 +0100

    s06-io-stream/s06-echo-server.cpp: added changes to file

--- s06-io-stream/s06-echo-server.cpp
+++ s06-io-stream/s06-echo-server.cpp
@@ -6,22 +6,22 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include <array>
 #include <iostream>
 #include <string>
 #include <vector>
-#include <array>
 
 auto main() -> int
 {
     // stworzenie nowego cocketu - nasłuchiwanie na połączenie albo
     // nazwiązywanie połączenia z kilentem
     auto sock = socket(AF_INET, SOCK_STREAM, 0);
-    
-    auto const ip = std::string{"127.0.0.1"};
+
+    auto const ip   = std::string{"127.0.0.1"};
     auto const port = uint16_t{8080};
 
     std::cout << "ip: " << ip << "\n";
-    std::cout << "port: " << port << "\n";  
+    std::cout << "port: " << port << "\n";
 
     // określenie połączenia
     sockaddr_in addr;
@@ -32,39 +32,36 @@ auto main() -> int
 
     // przygotowanie połączenia
     bind(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
-    
+
 
     // nasłuchiwanie na połączenie
     listen(sock, SOMAXCONN);
     auto client_sock = accept(sock, nullptr, nullptr);
 
-    close(sock);
-    
-    std::array<char, 4096> buf {0};
-    while (true){
 
+    std::array<char, 4096> buf{0};
+    while (true) {
         int bytesReceived = read(client_sock, buf.data(), buf.size());
         std::cout << std::string{buf.data(), static_cast<size_t>(4096)};
-        
-        if (bytesReceived == -1)
-        {
+
+        if (bytesReceived == -1) {
             perror("Error. Quitting\n");
             break;
         }
-        if (bytesReceived == 0)
-        {
+        if (bytesReceived == 0) {
             std::cout << "Client disconnected\n";
             break;
         }
 
-        //std::cout << std::string(buf, 0, bytesReceived) << std::endl;
+        // std::cout << std::string(buf, 0, bytesReceived) << std::endl;
         write(client_sock, buf.data(), buf.size());
-
     }
 
     shutdown(sock, SHUT_RDWR);
     // koniec połączenia
     close(client_sock);
+    close(sock);
+
 
     return 0;
 }
